1039dd8bc82fdaeef029d8548d7d24dd7a781d75
Marc d'Entremont <cramhead@gmail.com>
2014-01-20 16:42:05 -0800

    Added support of Modulus’ cloud storage. Note: this is a hack as a stop gap until the next version of collectionFS

--- cfs-public-folder.js
+++ cfs-public-folder.js
@@ -1,8 +1,15 @@
 var connect = Npm.require('connect');
 var path = Npm.require('path');
+var rootDir;
 
-var bundleRoot = path.join(process.mainModule.filename, '..');
-var rootDir = path.join(bundleRoot, '..') + path.sep;
+if (!process.env.CLOUD_DIR) {
+    var bundleRoot = path.join(process.mainModule.filename, '..');
+	rootDir = path.join(bundleRoot, '..') + path.sep;
+} else {
+    rootDir = process.env.CLOUD_DIR;
+}
+
+
 var serverPath = path.join(rootDir, 'cfs');
 
 var webPath = "/cfs";
@@ -10,9 +17,8 @@ var webPath = "/cfs";
 RoutePolicy.declare(webPath, 'network');
 
 WebApp
-	.connectHandlers
-	.use(
-	 	webPath, 
-		connect.static(serverPath)
-	);
-
+    .connectHandlers
+    .use(
+        webPath,
+        connect.static(serverPath)
+);
